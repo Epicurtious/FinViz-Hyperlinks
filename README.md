2ed6d0f38844623e9defe4cbefb14abba4dc4e63
Curtis Goodwin <curtisgoodwin117@yahoo.com>
2019-08-07 16:13:03 -0400

    Appends to csv file

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ How many stocks do you want scraped at a time? Like the first 30? Since it refre
 Project progression:
 CHECK Get news link from one stock if it’s the same day
 
-Get news links from all stocks if it’s the same day
+CHECK Get news links from all stocks if it’s the same day
 
-Compile information into csv file
+CHECK Compile information into csv file
 
-Make info append into csv file each time it runs
+CHECK Make info append into csv file each time it runs
 
 Extra: display current day info on webpage for easy access
--- WebScraper.py
+++ WebScraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import datetime
+import csv
 
 months = {
     1:"Jan",
@@ -21,7 +22,7 @@ todayDateTime = datetime.datetime.today()
 year = todayDateTime.year - 2000
 today = months[todayDateTime.month] + todayDateTime.strftime("-%d-") + "%d" % year
 urlBase = 'https://elite.finviz.com/'
-url1 = requests.get('https://elite.finviz.com/screener.ashx?v=152&f=cap_smallunder,sh_price_u20,ta_perf_dup&ft=4&o=-change&c=1,4,6,25,61,64')
+url1 = requests.get('https://elite.finviz.com/screener.ashx?v=152&f=cap_smallunder,sh_price_u20,ta_perf_dup&ft=4&o=-change&c=1,4,6,25,60,61,64,65')
 soup = bs(url1.content, 'lxml')
 table = soup.find(id='screener-content')
 rowLink = []
@@ -32,8 +33,19 @@ for row in table.findAll(True, {'class':['table-dark-row-cp', 'table-light-row-c
     news = soup2.find(class_='fullview-news-outer')
     date = news.td.text.split()[0]
     if date == today:
-        print(news.a.text)
-        print(news.a['href'])
-        for text in info:
-            print(text.text)
-        print
+        price = float(info[7].text)
+        change = float(info[4].text.strip('%'))/100
+        openPrice = round(price*(1-change),2)
+        priceStr = '$' + info[7].text
+        openPriceStr = '$' + "%f" % openPrice
+        ticker = info[0].text
+        industry = info[1].text
+        marketCap = info[2].text
+        sharesFloat = info[3].text
+        gap = info[5].text
+        relativeVolume = info[6].text
+        headline = news.a.text
+        newsLink = news.a['href']
+        with open('dataTest.csv', mode='a') as data:
+            writer = csv.writer(data)
+            writer.writerow([ticker,industry,openPriceStr,marketCap,sharesFloat,gap,priceStr,relativeVolume,headline,newsLink])
